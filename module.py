e559bf1933f88bc651aa6efa60cd87513129a04a
Dipendra Marasini <55050901+dipendramarasini@users.noreply.github.com>
2021-06-06 01:38:02 +0545

    Create module.py

---
+++ module.py
@@ -0,0 +1,9 @@
+a=float(input("enter the value of a"))
+b = float(input("enter the value of b"))
+if(a>b):
+    print("a is greater than b")
+elif(a==b):
+    print("a is equal to b")
+else:
+    print("b is greater than a")
+    
